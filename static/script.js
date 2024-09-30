fc9f89d22230c67ff7fb140f565d6f894e01f351
Luiz Felipe Sá <115410124+28lfss@users.noreply.github.com>
2024-09-30 15:23:39 -0300

    Main page upload (added pagination)
    Pagination is almost complete, just need to make the "Previous" & "Next" buttons work, and highlight the current page.

--- static/script.js
+++ static/script.js
@@ -15,61 +15,11 @@ fetch("https://restcountries.com/v3.1/all", {
     for (let i = 0; i < apiData.length; i++) {
         if (apiData[i].subregion === undefined) apiData[i].subregion = "—"
     }
-
-    cardsList()
-    nameCondition = 1
+    createPagination()
+    goToPage(0)
+    nameCondition = 0
 })
 
-function filterRegion (regionName) {
-    currentCountryList = []
-    var parameter, filter, txtValue
-    parameter = regionName
-    filter = parameter.toUpperCase()
-
-    apiData.sort((a,b) => a.name.common.localeCompare(b.name.common))
-
-    for (let i = 0; i < apiData.length - 1; i++) {
-            txtValue = apiData[i].region
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            currentCountryList.push(apiData[i])
-        }
-    }
-    searchResultList()
-}
-
-function filterSubRegion (subRegionName) {
-    currentCountryList = []
-    var parameter, filter, txtValue
-    parameter = subRegionName
-    filter = parameter.toUpperCase()
-
-    apiData.sort((a,b) => a.name.common.localeCompare(b.name.common))
-
-    for (let i = 0; i < apiData.length - 1; i++) {
-            txtValue = apiData[i].subregion
-            console.log(txtValue)
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            currentCountryList.push(apiData[i])
-        }
-    }
-    searchResultList()
-}
-
-function filterPopulation (min, max) {
-    currentCountryList = []
-    var minPopulation, maxPopulation, populationValue
-    minPopulation = min
-    maxPopulation = max
-    apiData.sort((a,b) => a.name.common.localeCompare(b.name.common))
-
-    for (let i = 0; i < apiData.length - 1; i++) {
-            populationValue = apiData[i].population
-        if (minPopulation <= populationValue && (populationValue < maxPopulation || maxPopulation === 'unlimited')) {
-            currentCountryList.push(apiData[i])
-        }
-    }
-    searchResultList()
-}
 
 ///// NAVIGATION BY SEARCH FUNCTIONS /////
 
@@ -88,162 +38,205 @@ function navigationBySearch (event) {
             currentCountryList.push(apiData[i])
         }
     }
-    searchResultList()
+    createPagination()
+    goToPage(0)
 }
 
 function searchResultList () {
-    var cardContainer = document.getElementById("card-container");
-    cardContainer.innerHTML = ''; // Clear all cards
+    clearCardContainer() // Clear all cards
 
-    for (let i = 0; i < currentCountryList.length ; i++) {
-        createCountryCard(
-            currentCountryList[i].name.common,
-            currentCountryList[i].flags.svg,
-            currentCountryList[i].region,
-            currentCountryList[i].subregion,
-            currentCountryList[i].population,
-            currentCountryList[i].area,
-        )
-    }
+    createPagination()
+    goToPage(0)
 }
+
 
 ///// SORTING FUNCTIONS & VARIABLES /////
 
-let nameCondition = 0
-let regionsCondition = 0
-let subRegionsCondition = 0
-let populationCondition = 0
-let areaCondition = 0
+// Sorting conditions
+let currentFilterCondition = 0
+let nameCondition = 1
+let regionsCondition = 1
+let subRegionsCondition = 1
+let populationCondition = 1
+let areaCondition = 1
 
 // Sorting by Name
 function sortNames() {
-    var cardContainer = document.getElementById("card-container");
-    cardContainer.innerHTML = ''; // Clear all cards
+    regionsCondition = 1
+    subRegionsCondition = 1
+    populationCondition = 1
+    areaCondition = 1
 
+    currentFilterCondition = nameCondition === 0 ? 1 : 0
+    nameCondition = currentFilterCondition
 
+    clearCardContainer()
     currentCountryList.sort((a,b) => a.name.common.localeCompare(b.name.common))
 
-    if (nameCondition === 0) {
-        cardsList()
-        nameCondition = 1
-    } else {
-        cardsListReversed()
-    }
-
+    createPagination()
+    goToPage(0)
 }
 
 // Sorting by Region
 function sortRegions() {
-    var cardContainer = document.getElementById("card-container");
-    cardContainer.innerHTML = ''; // Clear all cards
+    nameCondition = 1
+    subRegionsCondition = 1
+    populationCondition = 1
+    areaCondition = 1
 
+    currentFilterCondition = regionsCondition === 0 ? 1 : 0
+    regionsCondition = currentFilterCondition
+
+    clearCardContainer()
     currentCountryList.sort((a,b) => a.region.localeCompare(b.region))
 
-    if (regionsCondition === 0) {
-        cardsList()
-        regionsCondition = 1
-    } else {
-        cardsListReversed()
-    }
+    createPagination()
+    goToPage(0)
 }
 
 // Sorting by Sub Regions
 function sortSubRegions() {
-    var cardContainer = document.getElementById("card-container");
-    cardContainer.innerHTML = '';
+    nameCondition = 1
+    regionsCondition = 1
+    populationCondition = 1
+    areaCondition = 1
 
+    currentFilterCondition = subRegionsCondition === 0 ? 1 : 0
+    subRegionsCondition = currentFilterCondition
+
+    clearCardContainer()
     currentCountryList.sort((a,b) => {
         if (a.subregion === "—") return 1;  // Move 'a' down
         if (b.subregion === "—") return -1; // Move 'b' down
         return a.subregion.localeCompare(b.subregion);
     })
 
-    if (subRegionsCondition === 0) {
-        cardsList()
-        subRegionsCondition = 1
-    } else {
-        cardsListReversed()
-    }
+    createPagination()
+    goToPage(0)
 }
 
 // Sorting by Population
 function sortPopulation() {
-    var cardContainer = document.getElementById("card-container");
-    cardContainer.innerHTML = ''; // Clear all cards
+    nameCondition = 1
+    regionsCondition = 1
+    subRegionsCondition = 1
+    areaCondition = 1
 
+    currentFilterCondition = populationCondition === 0 ? 1 : 0
+    populationCondition = currentFilterCondition
+
+    clearCardContainer()
     currentCountryList.sort((a,b) => {
         if (a.population === "—") return 1;
         if (b.population === "—") return -1;
         return b.population - a.population
     })
-
-    if (populationCondition === 0) {
-        cardsList()
-        populationCondition = 1
-    } else {
-        cardsListReversed()
-    }
+    createPagination()
+    goToPage(0)
 }
 
 // Sorting by Area
 function sortArea() {
-    var cardContainer = document.getElementById("card-container");
-    cardContainer.innerHTML = ''; // Clear all cards
+    nameCondition = 1
+    regionsCondition = 1
+    subRegionsCondition = 1
+    populationCondition = 1
 
+    currentFilterCondition = areaCondition === 0 ? 1 : 0
+    areaCondition = currentFilterCondition
+
+    clearCardContainer()
     currentCountryList.sort((a,b) => {
         if (a.area === "—") return 1;
         if (b.area === "—") return -1;
         return b.area - a.area
     })
 
-    if (areaCondition === 0) {
-        cardsList()
-        areaCondition = 1
+    createPagination()
+    goToPage(0)
+}
+
+
+///// FILTERS FUNCTIONS /////
+
+// Filter by Region
+function filterRegion (regionName) {
+    currentCountryList = []
+    var parameter, filter, txtValue
+    parameter = regionName
+    filter = parameter.toUpperCase()
+
+    apiData.sort((a,b) => a.name.common.localeCompare(b.name.common))
+
+    for (let i = 0; i < apiData.length - 1; i++) {
+            txtValue = apiData[i].region
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            currentCountryList.push(apiData[i])
+        }
+    }
+    createPagination()
+    goToPage(0)
+}
+
+// Filter by Sub Region
+function filterSubRegion (subRegionName) {
+    currentCountryList = []
+    var parameter, filter, txtValue
+    parameter = subRegionName
+    filter = parameter.toUpperCase()
+
+    apiData.sort((a,b) => a.name.common.localeCompare(b.name.common))
+
+    for (let i = 0; i < apiData.length - 1; i++) {
+            txtValue = apiData[i].subregion
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            currentCountryList.push(apiData[i])
+        }
+    }
+    createPagination()
+    goToPage(0)
+}
+
+// Filter by Population amount
+function filterPopulation (min, max) {
+    currentCountryList = []
+    var minPopulation, maxPopulation, populationValue
+    minPopulation = min
+    maxPopulation = max
+    apiData.sort((a,b) => a.name.common.localeCompare(b.name.common))
+
+    for (let i = 0; i < apiData.length - 1; i++) {
+            populationValue = apiData[i].population
+        if (minPopulation <= populationValue && (populationValue < maxPopulation || maxPopulation === 'unlimited')) {
+            currentCountryList.push(apiData[i])
+        }
+    }
+    createPagination()
+    goToPage(0)
+}
+
+
+///// GENERATING CARDS & PAGINATION /////
+
+// Create the cards based in the current list defined by goToPage() function
+function cardsList (i) {
+    let stopLoop = 'break'
+    if (currentCountryList[i] === undefined) {
+        return stopLoop
     } else {
-        cardsListReversed()
-    }
-}
-
-// Default order list
-function cardsList () {
-    for (let i = 0; i < currentCountryList.length; i++) {
         createCountryCard(
-            currentCountryList[i].name.common,
             currentCountryList[i].flags.svg,
+            currentCountryList[i].name.common,
             currentCountryList[i].region,
             currentCountryList[i].subregion,
             currentCountryList[i].population,
             currentCountryList[i].area,
         )
     }
-    nameCondition = 0
-    regionsCondition = 0
-    subRegionsCondition = 0
-    populationCondition = 0
-    areaCondition = 0
 }
 
-// Reverse order list
-function cardsListReversed () {
-    for (let i = currentCountryList.length - 1; i > -1 ; i--) {
-        createCountryCard(
-            currentCountryList[i].name.common,
-            currentCountryList[i].flags.svg,
-            currentCountryList[i].region,
-            currentCountryList[i].subregion,
-            currentCountryList[i].population,
-            currentCountryList[i].area,
-        )
-    }
-    nameCondition = 0
-    regionsCondition = 0
-    subRegionsCondition = 0
-    populationCondition = 0
-    areaCondition = 0
-}
-
-// Creating card model
-function createCountryCard(countryName, flagUrl, region, subRegion, population, area) {
+// Create the card HTML elements
+function createCountryCard(flagUrl, countryName, region, subRegion, population, area) {
         var cardContainer = document.getElementById("card-container")
 
         var cardDiv = document.createElement("div") // class "card"
@@ -276,6 +269,65 @@ function createCountryCard(countryName, flagUrl, region, subRegion, population, 
         cardDiv.appendChild(cardBody)
         cardContainer.appendChild(cardDiv)
 }
+
+// Used to clear all the cards inside "card-container"
+function clearCardContainer() {
+    var cardContainer = document.getElementById("card-container")
+    cardContainer.innerHTML = ''
+}
+
+// Get the amount of pages necessary
+function amountPages() {
+    var listLength = currentCountryList.length
+    var pages = Math.floor(listLength / 12)
+    if ((listLength % 12) > 0) pages += 1
+    return pages
+}
+
+// Create the pagination HTML elements per page
+function createPagination() {
+    var pagination = document.getElementById('pagination')
+    pagination.innerHTML = ''
+    const pages = amountPages()
+
+    for (let i = 0; i < pages; i++) {
+        const newLi = document.createElement('li')
+        newLi.className = 'page-item'
+        newLi.id = `page-${i + 1}`
+        newLi.innerHTML = `<a class="page-link" href="#" onclick="goToPage(${i})">${i + 1}</a>`
+        const nextPage = document.getElementById('next-page')
+        pagination.appendChild(newLi)
+    }
+}
+
+// Show the chosen page
+function goToPage(pageNumber) {
+    clearCardContainer()
+
+    var activePage = document.getElementsByClassName('active')
+
+    let firstArrValue, lastArrValue
+    firstArrValue = pageNumber === 0 ? 0 : (pageNumber * 12)
+    lastArrValue = (pageNumber + 1) * 12
+
+    if (currentFilterCondition === 0) {
+        for(firstArrValue; firstArrValue < lastArrValue; firstArrValue++) {
+            let result = cardsList(firstArrValue)
+            if (result === 'break') {
+                break
+            }
+        }
+    } else {
+        currentCountryList.reverse()
+        for(firstArrValue; firstArrValue < lastArrValue; firstArrValue++) {
+            let result = cardsList(firstArrValue)
+            if (result === 'break') {
+                break
+            }
+        }
+    }
+}
+
 /*  DB IMPLEMENTATION
     fetch(
         "/api/post_country_list",{
